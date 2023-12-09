1cc1cc687a9aa2742fec481c137a67c7403502ed
lamchatka <maxprolife@icloud.com>
2023-12-09 15:50:07 +0300

    find-the-remainder.js

---
+++ 8kyu/find-the-remainder.js
@@ -0,0 +1,35 @@
+// DESCRIPTION:
+// Task:
+// Write a function that accepts two integers and returns the remainder of dividing the larger value by the smaller value.
+
+// Division by zero should return NaN.
+
+// Examples:
+// n = 17
+// m = 5
+// result = 2 (remainder of `17 / 5`)
+
+// n = 13
+// m = 72
+// result = 7 (remainder of `72 / 13`)
+
+// n = 0
+// m = -1
+// result = 0 (remainder of `0 / -1`)
+
+// n = 0
+// m = 1
+// result - division by zero (refer to the specifications on how to handle this in your language)
+
+
+// SOLUTION:
+function remainder(n, m){
+  
+    const max = Math.max(n, m);
+    const min = Math.min(n, m);
+    
+    return min === 0 ? NaN : max % min;   
+}
+
+console.log("Solution-1:", remainder(17,5));
+console.log("Solution-2:", remainder(13, 72));
