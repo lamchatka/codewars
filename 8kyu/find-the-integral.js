90ae187847474d71d6c170f381914fc562537b25
lamchatka <maxprolife@icloud.com>
2023-12-10 14:07:40 +0300

    find-the-integral.js

---
+++ 8kyu/find-the-integral.js
@@ -0,0 +1,31 @@
+
+// DESCRIPTION:
+// Create a function that finds the integral of the expression passed.
+
+// In order to find the integral all you need to do is add one to the exponent (the second argument), and divide the coefficient (the first argument) by that new number.
+
+// For example for 3x^2, the integral would be 1x^3: we added 1 to the exponent, and divided the coefficient by that new number).
+
+// Notes:
+
+// The output should be a string.
+// The coefficient and exponent is always a positive integer.
+// Examples
+//  3, 2  -->  "1x^3"
+// 12, 5  -->  "2x^6"
+// 20, 1  -->  "10x^2"
+// 40, 3  -->  "10x^4"
+// 90, 2  -->  "30x^3"
+
+
+
+// SOLUTION:
+function integrate(coefficient, exponent) {
+    return `${coefficient/(exponent+1)}x^${exponent+1}`
+}
+
+console.log("Solution-1: ", integrate(3, 2));
+console.log("Solution-2: ", integrate(12, 5));
+console.log("Solution-3: ", integrate(20, 1));
+console.log("Solution-4: ", integrate(40, 3));
+console.log("Solution-5: ", integrate(90, 2));
