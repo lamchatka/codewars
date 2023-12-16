b18ef91ff5211040e5753b311e67aa6aba003fc8
lamchatka <maxprolife@icloud.com>
2023-12-16 23:46:38 +0300

    take-the-first-n-elements.js

---
+++ 8kyu/take-the-first-n-elements.js
@@ -0,0 +1,12 @@
+// DESCRIPTION:
+// Create a function that accepts a list/array and a number n, and returns a list/array of the first n elements from the list/array.
+
+// If you need help, here's a reference:
+
+
+// SOLUTION:
+function take(arr, n) {
+    return arr.slice(0, n);
+}
+
+console.log("Solution-1: ", take([0, 1, 2, 3, 5, 8, 13], 3));
